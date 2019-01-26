ca569d4e8894b8c902aff86e261c5e25c68bba18
seekskywang <seekskywang@gmail.com>
2019-01-26 16:45:43 +0800

    0126

--- Include/usart.h
+++ Include/usart.h
@@ -10,6 +10,13 @@
 
 #ifndef _usart_h_
 #define _usart_h_
+#define USART_REC_LEN  			400*1024 //定义最大接收字节数 55K
+#define EN_USART1_RX 			1		//使能（1）/禁止（0）串口1接收
+
+
+extern u16 USART_RX_STA; //?????? 
+extern u32 USART_RX_CNT;  //?????? 
+
 
 void USART_Configuration(void);//串口初始化函数
 void UART1_Send(void);
